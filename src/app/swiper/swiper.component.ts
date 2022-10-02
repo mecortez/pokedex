6c3c1dc0df8e78abb7dcd4545d4f9590eceffb21
mecortez <drcorteez@gmail.com>
2022-10-02 17:18:07 -0300

    swiper

--- src/app/swiper/swiper.component.ts
+++ src/app/swiper/swiper.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Virtual } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+SwiperCore.use([Virtual]);
+
 @Component({
   selector: 'app-swiper',
   templateUrl: './swiper.component.html',
@@ -11,5 +19,11 @@ export class SwiperComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  // Cria array com 3 slides
+  slides = Array.from({ length: 3 }).map(
+    (el, index) => `Slide ${index + 1}`
+  );
+
 
 }
