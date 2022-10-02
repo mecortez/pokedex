ecaf601eaa4defc8daa1daceaee4d9d9d07489b8
mecortez <drcorteez@gmail.com>
2022-10-02 16:00:54 -0300

    dev inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SwiperComponent } from './swiper/swiper.component';
+import { SwiperModule } from 'swiper/angular';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
